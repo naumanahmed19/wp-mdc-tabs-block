9b2ae031a519574fd13f103e501edec36b81c360
Nomi <naumanahmed19@gmail.com>
2022-06-12 17:06:29 +0200

    enh: nested tabs support

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
-
+//TODO: Refactoring...
 document.addEventListener("DOMContentLoaded", function() {
+  
 
     const ele = document.querySelectorAll('.mdc-tab-bar')
     if(!ele) return
@@ -12,7 +13,9 @@ document.addEventListener("DOMContentLoaded", function() {
       contentEls[0].classList.add('brand-tab-screen--active');
       tabBar.listen('MDCTabBar:activated', function(event) {
       // Hide currently-active content
-      document.querySelector('.brand-tab-screen--active').classList.remove('brand-tab-screen--active');
+
+      if(tabWrap[index].querySelector('.brand-tab-screen--active'))
+        tabWrap[index].querySelector('.brand-tab-screen--active').classList.remove('brand-tab-screen--active');
       // Show content for newly-activated tab
       contentEls[event.detail.index].classList.add('brand-tab-screen--active');
       });
