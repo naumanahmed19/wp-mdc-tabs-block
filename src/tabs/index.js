f6d184d9e1d12b260f6d946d14c13f9f5a838679
Nomi <naumanahmed19@gmail.com>
2022-06-12 11:18:09 +0200

    refactor: clean code

--- src/tabs/index.js
+++ src/tabs/index.js
@@ -21,14 +21,9 @@
  import save from './save';
  import metadata from './block.json';
  
- import { MDCTabBar } from '@material/tab-bar';
-
- 
  // Importing code libraries for this block
  import { __ } from '@wordpress/i18n';
- import { RichText } from '@wordpress/block-editor';
- import { Button } from '@wordpress/components';
- 
+
  /**
   * Every block starts by registering a new block type definition.
   *
@@ -44,21 +39,3 @@
       */
      save,
  } );
-
-
-
-//  window.onload = function() {
-//     console.log('testing..., bar',document.querySelector('.mdc-tab-bar'))
-
-//     var tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
-//     console.log('tabbar',tabBar)
-
-//     var contentEls = document.querySelectorAll('.brand-tab-screen');
-//     contentEls[0].classList.add('brand-tab-screen--active');
-//     tabBar.listen('MDCTabBar:activated', function(event) {
-//     // Hide currently-active content
-//     document.querySelector('.brand-tab-screen--active').classList.remove('brand-tab-screen--active');
-//     // Show content for newly-activated tab
-//     contentEls[event.detail.index].classList.add('brand-tab-screen--active');
-//     });
-//  } ;
